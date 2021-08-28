f09ac171fefeac7885429da3e962cc1f53b36d82
abhijit121 <abhijitkarmakar072@gmail.com>
2021-08-28 20:26:18 +0530

    tt

---
+++ practice.js
@@ -0,0 +1,37 @@
+//First Program
+//console.log('Abhijit Karmakar');
+//Second 
+//prompt('What is you name? ');
+//Third
+// var readLineSync = require('readline-sync');
+// var inputName = readLineSync.question('What colour do you love? ');
+// console.log(inputName);
+
+//Fourth
+// var readLineSync = require('readline-sync');
+// var inputName = readLineSync.question('What colour do yourname ? ');
+// console.log('Welcome '+inputName);
+// console.log(`Welcome ${inputName}`);
+
+//Fifth
+// var readLineSync = require('readline-sync');
+// var inputName = readLineSync.question('What is do your name ? ');
+// console.log('Welcome '+inputName);
+// console.log(`Welcome ${inputName}`);
+
+//Sixth
+
+// var readLineSync = require('readline-sync');
+// var answer = readLineSync.question('Are you greater than 25 years ? ');
+
+// if(answer.toLowerCase() === 'yes'){
+//   console.log('Right');
+// }
+// else{
+//   conosle.log('Wrong')
+// }
+
+//Seventh
+
+
+
